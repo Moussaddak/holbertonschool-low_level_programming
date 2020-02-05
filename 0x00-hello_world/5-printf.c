c5b5d6a4f111b9f23bb2c9ddc139803f5a020ad5
Moussaddak <meddeb.moussaddak@gmail.com>
2020-02-05 14:36:53 +0100

    Hello, Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - print function will display a message
+*Return: 0
+*/
+int main(void)
+{
+printf("Programming is like building a multilingual puzzlwith proper grammar, but the outcome is a piece of art,");
+return (0);
+}
