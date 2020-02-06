fd9e55c8439256cd5faab2351dfd1097d8203d0e
Moussaddak <meddeb.moussaddak@gmail.com>
2020-02-06 21:24:30 +0100

    aphabet uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main- main block
+ *Return: 0
+ */
+int main(void)
+{
+int c;
+for (c = 97; c <= 122; c++)
+{
+putchar(c);
+}
+for (c = 65; c <= 90; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
