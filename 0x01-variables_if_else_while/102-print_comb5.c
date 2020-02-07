b50d35fd15f4505ddb971eb8e5fe9a2b2711b98d
Moussaddak <meddeb.moussaddak@gmail.com>
2020-02-07 19:33:48 +0100

    combination 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ *main- main block
+ *Return: 0
+ */
+int main(void)
+{
+int c, k, f, e;
+for (c = 0; c <= 9; c++)
+{
+for (k = 0; k < 9; k++)
+{
+for (f = 0; f <= 9; f++)
+{
+for (e = 0; e <= 9; e++)
+{
+
+putchar(c + 48);
+putchar(k + 48);
+putchar(32);
+putchar(f + 48);
+putchar(e + 48);
+if (f == 9 && e == 9 && k < 9)
+{
+f = c;
+k++;
+e = k;
+}
+if (c == 9 && k == 8 && f == 9 && e == 9)
+break;
+putchar(',');
+putchar(32);
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
