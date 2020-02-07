e20529f4a2ca8a2b5688e374805d9e048cd9bd21
Moussaddak <meddeb.moussaddak@gmail.com>
2020-02-07 11:08:51 +0100

    other combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,16 +5,29 @@
  */
 int main(void)
 {
-int c, k, f;
+int c, k, f, x, y, z;
 for (c = 0; c < 8; c++)
 {
 for (k = c + 1; k < 9; k++)
 {
 for (f = c + 2; f <= 9; f++)
 {
-putchar(c + '0');
-putchar(k + '0');
-putchar(f + '0');
+x = c;
+y = k;
+z = f;
+if (c == x && k == z && f == y)
+f++;
+else if (c == y && k == x && f == z)
+f++;
+else if (c == z && k == x && f == y)
+f++;
+else if (c == y && k == z && f == x)
+f++;
+else if (c == z && k == y && f == x)
+f++;
+putchar(c + 48);
+putchar(k + 48);
+putchar(f + 48);
 if (c == 7 && k == 8 && f == 9)
 break;
 putchar(',');
