aadbc7dba986dbb708da6077065a91b71ccda10a
Moussaddak <52888741+Moussaddak@users.noreply.github.com>
2020-02-10 10:22:44 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,11 +21,18 @@ putchar(f + 48);
 putchar(e + 48);
 if (c == 9 && k == 8 && f == 9 && e == 9)
 break;
-if (f == 9 && e == 9 && k <= 9)
+if (f == 9 && e == 9 && k < 9)
 {
 f = c;
 k++;
 e = k;
+}
+else if (f == 9 && e == 9 && k == 9)
+{
+c++;
+f = c;
+k = 0;
+e = k++;
 }
 putchar(44);
 putchar(32);
