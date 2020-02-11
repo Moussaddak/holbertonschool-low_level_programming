da11eb47d22728535b98a413f921981c4693bdf5
Moussaddak <meddeb.moussaddak@gmail.com>
2020-02-11 09:12:45 +0100

    _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-char str[] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+char str[] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n', '\0'};
 int i, len;
 len = strlen(str);
-for (i = 0 ; i < len - 1; i++)
+for (i = 0 ; i < len ; i++)
 _putchar(str[i]);
 _putchar('\n');
 return (0);
