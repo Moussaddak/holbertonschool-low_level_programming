72acf391c2dc220d402edaa7df98a47a8850db1b
Moussaddak <meddeb.moussaddak@gmail.com>
2020-02-11 14:00:30 +0100

    prints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_sign - display +/-/0 according to number n
+ * @n: value to be checked
+ * Return: 1 or -1 or 0
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
+_putchar('\n');
+}
