0d9699f77883dc46972b3e172e41cfa9acade58c
Moussaddak <meddeb.moussaddak@gmail.com>
2020-02-11 22:07:08 +0100

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,29 +6,28 @@
 void times_table(void)
 {
 int i, j, k, n;
-for (i = 0; i < 10; i++)
+for (i = 0; i <= 9; i++)
 {
-k = 0;
+k = 1;
 _putchar('0');
-_putchar(',');
-for (j = 0; j < 10; j++)
+for (j = 0; j < 9; j++)
 {
 n = i * k;
-if (n > 9)
+if (n <= 9)
 {
-_putchar(' ');
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
-}
-else
-{
+_putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(n + '0');
 }
-if (j < 9)
+else 
+{
 _putchar(',');
-else
+_putchar(' ');
+_putchar((n / 10) + '0');
+_putchar((n % 10) + '0');
+}
+if (j == 8)
 _putchar('\n');
 k++;
 }
