6d71bcb3fb6b9b77122ddafa7410b165f65ed51a
Moussaddak <meddeb.moussaddak@gmail.com>
2020-02-11 09:44:12 +0100

    function print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+/**
+ * print_alphabet - function that print lowercase alphabet
+ *
+ * Return: Always 0.
+ */
+int _putchar(char c);
+void print_alphabet(void)
+{
+int i;
+char c;
+for (i = 97 ; i <= 122 ; i++)
+{
+c = i;
+_putchar(c);
+}
+_putchar('\n');
+}
