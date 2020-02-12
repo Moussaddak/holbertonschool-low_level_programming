36cb7b6483451a1d4191c827ca0dd427f61c6c72
Moussaddak <meddeb.moussaddak@gmail.com>
2020-02-12 22:12:24 +0100

    times tables of n

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,6 @@
 #include "holberton.h"
 /**
  *times_table - prints the n times table, starting with 0
- *@n: input
  *Return: 0
  */
 void print_times_table(int n)
@@ -16,18 +15,25 @@ _putchar('0');
 for (j = 0; j < n; j++)
 {
 q = i * k;
-if (q <= n)
+if (q <= 9)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(q + '0');
 }
-else
+else if (q > 9 && q <= 99)
 {
 _putchar(',');
 _putchar(' ');
 _putchar((q / 10) + '0');
+_putchar((q % 10) + '0');
+}
+else
+{
+_putchar(' ');
+_putchar((q / 100) + '0');
+_putchar(((q % 100)/10) + '0');
 _putchar((q % 10) + '0');
 }
 k++;
