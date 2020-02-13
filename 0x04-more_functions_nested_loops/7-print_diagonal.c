4208b98cbfbd5488e2ae1b36ef356a539e1ee412
Moussaddak <meddeb.moussaddak@gmail.com>
2020-02-13 16:11:27 +0100

    draws a diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,17 +6,24 @@
  */
 void print_diagonal(int n)
 {
-int i, j;
+int i, j, space;
 if (n > 0)
 {
-for (i = 0; i < n; i++)
+for (i = 1; i <= n; i++)
 {
-for (j = 0; j < n; j++)
+for (j = 1; j <= n; j++)
 {
 if (i == j)
+{
+space = j;
+while (--space)
+_putchar(' ');
 _putchar(92);
+_putchar('\n');
 }
 }
 }
+}
+else
 _putchar('\n');
 }
