f7e6a0394546ae83a954195dc6beb82c1422a26a
Moussaddak <meddeb.moussaddak@gmail.com>
2020-02-13 17:07:29 +0100

    display a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,21 +6,14 @@
  */
 void print_square(int size)
 {
-int i, j, k, nb_sq;
+int i, j;
 if (size > 0)
 {
 for (i = 1; i <= size; i++)
 {
 for (j = 1; j <= size; j++)
-{
-nb_sq = j + 1;
-for (k = 0; k < nb_sq ; k++)
-{
-while (--nb_sq)
 _putchar('#');
 _putchar('\n');
-}
-}
 }
 }
 else
