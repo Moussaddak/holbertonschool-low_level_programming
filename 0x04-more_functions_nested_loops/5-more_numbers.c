bb8645870eba3e0af2f8dcf012c43bd486677606
Moussaddak <meddeb.moussaddak@gmail.com>
2020-02-13 14:26:44 +0100

    print 10 time numbers from 0..14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *more_numbers- prints the numbers from 0 to 9, ten times
+ *Return: always 0
+ */
+void more_numbers(void)
+{
+int i;
+for (j = 0; j < 10; j++)
+{
+for (i = 0; i <= 14; i++)
+{
+if (i > 9)
+_putchar((i / 10) + 48);
+_putchar((i % 10) + 48);
+}
+_putchar('\n');
+}
+}
