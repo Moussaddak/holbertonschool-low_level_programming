ca58b5e168e427ce1e42b6b8cc922cc44673c3b4
Moussaddak <meddeb.moussaddak@gmail.com>
2020-02-14 23:40:57 +0100

    largest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ *main- display the largest prime factor of number
+ *Return: 0
+ */
+int main(void)
+{
+int i, j, test, prime;
+long n;
+n = 612852475143;
+prime = 1;
+test = 1;
+j = 2;
+while (test)
+{
+i = ++prime;
+/* test if i is prime number */
+/* find a prime number*/
+do {
+if ((i % j) != 0)
+j++;
+else
+/* if not a prime increment by 1*/
+prime = i;
+} while (j < i);
+/* search of the largest factor prime*/
+while ((n % prime) == 0)
+{
+n /= prime;
+if (n == 1)
+test = 0;
+}
+}
+printf("%d", prime);
+return (0);
+}
