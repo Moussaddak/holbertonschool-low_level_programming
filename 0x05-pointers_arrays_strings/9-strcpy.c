6331fdf5c7507b08f02e603e8c9332bdd6ad5623
Moussaddak <meddeb.moussaddak@gmail.com>
2020-02-18 23:22:25 +0100

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+/**
+ *_strcpy - prints n elements of an array of integers
+ *@dest: input
+ *@src: input
+ *Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+for (i = 0; *src != '\0'; i++)
+{
+*dest = *src;
+dest++;
+src++;
+}
+return (dest);
+}
