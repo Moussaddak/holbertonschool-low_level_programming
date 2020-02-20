4d7c1e5940d5f50d8d7e1d074a53e7eabe33451c
Moussaddak <meddeb.moussaddak@gmail.com>
2020-02-20 16:27:30 +0100

    copy the first n characters of the src into dest

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,14 +10,17 @@ char *_strncpy(char *dest, char *src, int n)
 int i = 0;
 char *r = dest;
 
-while (*src != '\0' && i != n)
+while (*src != '\0' && i < n)
 {
 *dest = *src;
 src++;
 dest++;
 i++;
 }
-for (; i < n; i++)
+for(; i < n; i++)
+{
 *dest = '\0';
+dest++;
+}
 return (r);
 }
