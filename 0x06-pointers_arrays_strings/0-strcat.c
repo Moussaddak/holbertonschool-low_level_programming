1118bc5e742017dcb46d264353f25ae34263a8fc
Moussaddak <meddeb.moussaddak@gmail.com>
2020-02-20 10:57:36 +0100

    Concatinate two strings together

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+/**
+ *_strcat - concatinate two strings together
+ *@str: input
+ *@dest: input
+ *Return: a string 
+ */
+char *_strcat(char *dest, char *src)
+{
+char *str = dest;
+while (*dest != '\0')
+dest++;
+while (*src != '\0')
+{
+*dest = * src;
+dest++;
+src++;
+}
+return(str);
+}
