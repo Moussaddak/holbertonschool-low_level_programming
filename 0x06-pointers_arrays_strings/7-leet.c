36df1d7dba74e3c63db2519ba7b7f37a5630acb0
Moussaddak <meddeb.moussaddak@gmail.com>
2020-02-20 23:40:53 +0100

    encode a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,18 +6,19 @@
 char *leet(char *s)
 {
 char *r = s;
+char lower[6] = {'a', 'e', 'o', 't', 'l'};
+char upper[6] = {'A', 'E', 'O', 'T', 'L'};
+char code[6] = {'4', '3', '0', '7', '1'};
+int i;
 while (*s != '\0')
 {
-if (*s == 'a' || *s == 'A')
-*s = '4';
-else if (*s == 'e' || *s == 'E')
-*s = '3';
-else if (*s == 'o' || *s == 'O')
-*s = '0';
-else if (*s == 't' || *s == 'T')
-*s = '7';
-else if (*s == 'l' || *s == 'L')
-*s = '1';
+i = 0;
+while (i < 5)
+{
+if (*s == upper[i] || *s == lower[i])
+*s = code[i];
+i++;
+}
 s++;
 }
 return (r);
