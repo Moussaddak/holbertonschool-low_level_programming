f0eeec39784c958ef09dbe7a8fe97483ce532a01
Moussaddak <meddeb.moussaddak@gmail.com>
2020-02-21 14:33:45 +0100

    New correction

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,34 +5,34 @@
  */
 char *cap_string(char *s)
 {
-char l[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\n', '\t', ' '};
-char *r = s;
-int i, test;
-if (*s >= 97 && *s <= 122)
-{
-*s -= 32;
-s++;
-}
-while (*s != '\0')
-{
-i = 0;
-test = 1;
-while (i < 13 && test)
-{
-if (*s == l[i])
-{
-s++;
-if (*s >= 97 && *s <= 122)
-{
-*s -= 32;
-test = 0;
-}
-else
-test = 1;
-}
-i++;
-}
-s++;
-}
-return (r);
+	char l[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\n', '\t',
+		    ' '};
+	char *r = s;
+	int i, test;
+
+	if (*s >= 97 && *s <= 122)
+		*s -= 32;
+	s++;
+	while (*s != '\0')
+	{
+		i = 0;
+		test = 1;
+		while (i < 15 && test)
+		{
+			if (*s == l[i])
+			{
+				s++;
+				if (*s >= 97 && *s <= 122)
+				{
+					*s -= 32;
+					test = 0;
+				}
+				else
+					test = 1;
+			}
+			i++;
+		}
+		s++;
+	}
+	return (r);
 }
