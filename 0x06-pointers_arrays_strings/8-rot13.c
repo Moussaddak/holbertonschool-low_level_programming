e20d305d93b83ac9d7b6647de2fbec05ee185e50
Moussaddak <meddeb.moussaddak@gmail.com>
2020-02-21 10:05:45 +0100

    encrypt a string

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -6,17 +6,22 @@
 char *rot13(char *s)
 {
 char *r = s;
-int i;
-char *code = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-char *alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i, test = 1;
+
+char *alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char *code = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 while (*s != '\0')
 {
 i = 0;
-while (i < 53)
+test = 1;
+while (i < 53 && test)
 {
 if (*s == alpha[i])
+{
 *s = code[i];
+test = 0;
+}
 i++;
 }
 s++;
