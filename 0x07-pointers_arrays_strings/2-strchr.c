a77474d4210c945a15a14baf3204681acf5cb1ed
Moussaddak <52888741+Moussaddak@users.noreply.github.com>
2020-02-24 19:37:28 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,17 +7,16 @@
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i = 0, br = 1;
+	unsigned int i = 0;
 	char *r;
 
 	r = NULL;
 
-	while (((*(s + i) != '\0' || *(s + i) == '\0')) && br)
+	while (*(s + i) != '\0')
 	{
 		if (*(s + i) == c)
 		{
 			r = s + i;
-			br = 0;
 		}
 		i++;
 	}
