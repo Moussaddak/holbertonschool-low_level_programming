f1879ba03cf7c0475b54c05c4e194951a28025de
Moussaddak <meddeb.moussaddak@gmail.com>
2020-02-24 12:47:28 +0100

    function that copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+/**
+ *_memcpy - copies memory area
+ *@dest: input
+ *@src: input
+ *@n: input
+ *Return: string
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (*src != '\0' && i < n)
+	{
+		*(dest + i) = *src;
+		src++;
+		i++;
+	}
+	return (dest);
+}
