dedbacc2c46af04adf1674f1e4357f548d49dccd
Moussaddak <meddeb.moussaddak@gmail.com>
2020-02-24 20:32:23 +0100

    function locate a character in string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+/**
  *_strchr - locates a character in a string.
  *@s: input
  *@c: input
@@ -16,6 +18,5 @@ char *_strchr(char *s, char c)
 		if (*(s + i) == c)
 			r = s + i;
 	}
-	
 	return (r);
 }
