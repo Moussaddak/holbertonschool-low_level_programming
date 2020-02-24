9739a343a2d491526f1ba81bcf42ea5279e1f711
Moussaddak <meddeb.moussaddak@gmail.com>
2020-02-25 00:27:51 +0100

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,13 +4,13 @@
  *@a: input
  *Return: nothing
  */
-void print_chessboard(char *a[8][8])
+void print_chessboard(char (*a)[8])
 {
 int i, j;
 for (i = 0; i < 8; i++)
 {
 for (j = 0; j < 8; j++)
-_putchar(a[i][j]);
+_putchar((a[i][j]));
 _putchar('\n');
 }
 }
