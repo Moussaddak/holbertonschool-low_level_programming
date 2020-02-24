5e556e51ed37fe651b2af30eb8a92224fb754296
Moussaddak <meddeb.moussaddak@gmail.com>
2020-02-24 20:09:28 +0100

    function locate a character in string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,19 +7,16 @@
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i = 0, br = 1;
+	int i = 0;
 	char *r;
 
 	r = NULL;
-
-	while (((*(s + i) != '\0' || *(s + i) == '\0')) && br)
+	while (*(s + i) != '\0')
+	i++;
+	for (; i >= 0; i--)
 	{
 		if (*(s + i) == c)
-		{
-			r = s + i;
-			br = 0;
-		}
-		i++;
+		r = s + i;
 	}
 	return (r);
 }
