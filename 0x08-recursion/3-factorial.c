b5e6cc60b35f70f87c98f2a36aa0a3455537baed
Moussaddak <meddeb.moussaddak@gmail.com>
2020-02-26 21:16:24 +0100

    returns the factorial of a given number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,12 @@
+/**
+ *factorial - return the factorial of a given number
+ *@n: input
+ *Return: integer
+ */
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
