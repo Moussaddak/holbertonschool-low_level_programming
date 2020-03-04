2449dac6bacfbbcb31fcddd665e0e7661de0efe5
moussaddak <meddeb.moussaddak@gmail.com>
2020-03-04 21:41:39 +0000

    free 2 dimensional grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+/**
+ *free_grid -  frees a 2 dimensional grid
+ *@grid: input
+ *@height: input
+ */
+void free_grid(int **grid, int height)
+{
+int i;
+	for (i = 0; i < height; i++)
+	{
+		free(*(grid + i));
+	}
+}
