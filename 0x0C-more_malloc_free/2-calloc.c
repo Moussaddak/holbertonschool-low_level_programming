68afbaf8f2c809428c4bf464ee2c01f445c0b6ac
moussaddak <meddeb.moussaddak@gmail.com>
2020-03-05 21:24:44 +0000

    memory of array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+/**
+ *_calloc - creat an array
+ *@nmemb: input
+ *@size: input
+ *Return: array of a char
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+unsigned int i;
+char *p;
+
+	if (size == 0 || nmemb == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		p = malloc(nmemb * size);
+		if (p == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			for (i = 0; i < size; i++)
+			{
+				*(p + i) ='0';
+			}
+		}
+	}
+	return (p);
+}
