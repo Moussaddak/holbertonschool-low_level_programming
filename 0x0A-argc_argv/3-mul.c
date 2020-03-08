7d2f7cf73a437318b4aa8ce34d4b7b87a422e20c
moussaddak <meddeb.moussaddak@gmail.com>
2020-03-08 22:58:47 +0000

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main -  a program that multiplies two numbers.
+ *@argc : input
+ *@argv : input
+ *Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int sum = 0;
+
+	if (argc == 3)
+	{
+		sum = atoi(*(argv + 1)) * atoi(*(argv + 2));
+		printf("%d\n", s);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
