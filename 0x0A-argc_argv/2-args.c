114a20f8153f64feeb0fede7a7a1e78354dd1e68
moussaddak <meddeb.moussaddak@gmail.com>
2020-03-08 22:53:20 +0000

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - main function
+ *@argc: input
+ *@argv: input
+ *Return: always 0
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", *(argv + i));
+	}
+	return (0);
+}
