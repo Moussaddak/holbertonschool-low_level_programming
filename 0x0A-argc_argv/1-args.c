ed961d72c77d50f4430cedb87845c28adc2b9276
moussaddak <meddeb.moussaddak@gmail.com>
2020-03-08 22:44:23 +0000

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define UNUSED(argv) (void)(argv)
+/**
+ *main - main function
+ *@argc: input
+ *@argv: input
+ *Return: always 0
+ */
+int main(int argc, char **argv)
+{
+	UNUSED(argv);
+	printf("%d\n", argc);
+	return (0);
+}
