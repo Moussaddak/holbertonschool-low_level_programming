15945d3c187171c49da7f509bb42be7dd4e8c725
moussaddak <meddeb.moussaddak@gmail.com>
2020-03-09 20:37:14 +0000

    Django

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
