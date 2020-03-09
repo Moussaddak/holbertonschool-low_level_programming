f7032c9a6701939ab08ff4d784bf8b38e08420d0
moussaddak <meddeb.moussaddak@gmail.com>
2020-03-09 22:16:01 +0000

    free dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+include <stdio.h>
+#include "dog.h"
+/**
+ *free_dog - function frees elets of struct dog
+ *@d: input
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
