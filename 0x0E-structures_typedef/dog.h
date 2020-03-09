7b06f3ed5f324c900308e3ddca79eb5b574841ad
moussaddak <meddeb.moussaddak@gmail.com>
2020-03-09 20:50:01 +0000

    header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,11 @@
  *
  * Description: Longer description
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
