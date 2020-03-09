977a7dc30bdaa4d55d9d8824f87d29c77c20a2bd
moussaddak <meddeb.moussaddak@gmail.com>
2020-03-09 22:33:41 +0000

    New dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ *new_dog - function stores dog info
+ *@name: input
+ *@age: input
+ *@owner: input
+ *Return: type struct dog pointer
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+}
