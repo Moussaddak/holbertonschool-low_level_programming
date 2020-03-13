15e7b1c7c802e4bb76a9624bc34b17a24729d4c7
moussaddak <meddeb.moussaddak@gmail.com>
2020-03-13 09:34:10 +0000

    new instance dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include "dog.h"
-#include <string.h>
-/*char *_strdup(char *str);*/
+
+char *_strdup(char *str);
 
 /**
  *new_dog - function stores dog info
@@ -13,7 +13,6 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	/*char *n, *o;*/
 
 	dog = malloc(sizeof(dog_t));
 
@@ -22,11 +21,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	/*n = _strdup(name);
-	  o = _strdup(owner);*/
-	(*dog).name = strdup(name);
-	(*dog).owner = strdup(owner);
-	(*dog).age = age;
+	(*dog).name = _strdup(name);
+	(*dog).owner = _strdup(owner);
 
 	if ((*dog).name == NULL || (*dog).owner == NULL)
 	{
@@ -36,6 +32,45 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
+	(*dog).age = age;
 
 	return (dog);
+}
+
+/**
+ *_strdup -  duplicate of the string pointed to by str
+ *@str: input
+ *Return: string
+ */
+char *_strdup(char *str)
+{
+	int i = 0, j = 0;
+	char *cp;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+
+	cp = malloc(i * sizeof(char) + 1);
+
+	if (cp == NULL)
+	{
+		return (NULL);
+	}
+
+	while (*(str + j) != '\0')
+	{
+		(cp + j) = (str + j);
+		j++;
+	}
+
+	*(cp + j) = '\0';
+
+	return (cp);
 }
