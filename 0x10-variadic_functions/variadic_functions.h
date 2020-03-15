eec7d15408f74a682eaf7010a491d13997afc574
moussaddak <meddeb.moussaddak@gmail.com>
2020-03-15 21:23:22 +0000

    header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef VAR_FCT
 #define VAR_FCT
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
