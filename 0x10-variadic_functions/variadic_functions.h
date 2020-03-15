ce14e430d36ddf3943845f7bd617123831af4046
moussaddak <meddeb.moussaddak@gmail.com>
2020-03-15 00:58:44 +0000

    header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,4 +4,16 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+/**
+ * struct list - a list of types of arguments.
+ * @cr: First member
+ * @ty: Second member
+ *
+ * Description: a list of types of arguments passed to the function
+ */
+typedef struct list {
+	char c;
+	char *cr;
+	char *ty;
+}list;
 #endif
