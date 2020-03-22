c7d4a34725df65880fa48ce6c95a155734b23bda
moussaddak <meddeb.moussaddak@gmail.com>
2020-03-22 23:05:13 +0000

    first_print

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ *  first - a function that prints before the main function is executed.
+ */
+void first(void)__attribute__ ((constructor));
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
