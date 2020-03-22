7da6f19a8316943b5325d28b41c14548249ec90f
moussaddak <meddeb.moussaddak@gmail.com>
2020-03-22 01:56:04 +0000

    print_number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+#define ABS(x) ((x) < (0) ? (-(x)) : (x))
+/**
+ *print_number - print an integer
+ *@n: input integer argument
+ */
+void print_number(int n)
+{
+	int div = 1, i = 1, j;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = ABS(n);
+	}
+	if (n >= 0 && n <= 9)
+	{
+		_putchar(n + '0');
+	}
+	for (; n / div >= 10; div *= 10)
+	{
+		i++;
+	}
+	n_i = i - 2;
+	_putchar((n / _pow(10, i - 1)) + '0');
+	for (j = n_i; j >= 1; j--)
+	{
+		_putchar(((n / _pow(10, j)) % 10) + '0');
+	}
+	_putchar((n % 10) + '0');
+}
+/**
+ *_pow - returns the value of x raised to the power of y
+ *@x: input
+ *@y: input
+ *Return: integer
+ */
+int _pow(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	else
+		return (x * _pow(x, y - 1));
+}
