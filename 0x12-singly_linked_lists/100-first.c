8f2e650595a10b51e170400c1b2ce9ae5134721d
moussaddak <meddeb.moussaddak@gmail.com>
2020-03-22 23:09:01 +0000

    first_print

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
+#include "list.h"
 /**
  *  first - a function that prints before the main function is executed.
  */
-void first(void)__attribute__ ((constructor));
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
