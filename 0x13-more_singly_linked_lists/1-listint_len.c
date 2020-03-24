43c1cf0a921c1143d9c1e0d50b567a491d849c9d
moussaddak <meddeb.moussaddak@gmail.com>
2020-03-24 14:30:03 +0000

    number_of_elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ *listint_len -  returns the number of elements in a linked listint_t list
+ *@h: input of type list_t
+ *Return: the number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
