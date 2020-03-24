d0aed0d69563d1f65ca1d0e56df8c5981a15c464
moussaddak <meddeb.moussaddak@gmail.com>
2020-03-24 14:23:53 +0000

    print_all_elts_lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ *print_listint -  prints all the elements of a listint_t list
+ *@h: input of type list_t
+ *Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
