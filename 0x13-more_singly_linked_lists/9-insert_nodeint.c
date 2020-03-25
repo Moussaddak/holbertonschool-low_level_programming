a17763352051a25c5d7d20723f250ddee3ce829b
Moussaddak <52888741+Moussaddak@users.noreply.github.com>
2020-03-25 19:30:07 +0100

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *node = *head, *p_node = *head, *new;
 	unsigned int i = 0, j;
 
-	if (!*head)
+	if (!head)
 	{
 		return (NULL);
 	}
