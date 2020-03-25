823f895c2e49e1bde6ab326ce9035b2ff457e1a2
moussaddak <meddeb.moussaddak@gmail.com>
2020-03-25 17:18:31 +0000

    insert_a_node_in_specific_index_of_list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+listint_t *add_nodeint(listint_t **head, const int n);
 /**
  *insert_nodeint_at_index - inserts a new node at a given position.
  *@head: input of type listint_t
@@ -11,41 +12,66 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *node = *head, *p_node = *head, *new;
 	unsigned int i = 0, j;
 
-	new = malloc(sizeof(listint_t));
-	if (!new)
+	while (node)
+	{
+		node = node->next;
+		i++;
+	}
+	if (i == 0)
+	{
+		new = add_nodeint(head, 0);
+		return (new);
+	}
+	else if (i == idx)
+	{
+		new = add_nodeint_end(head, idx);
+		return (new);
+	}
+	else if (idx > i)
 	{
 		return (NULL);
 	}
 	else
 	{
-		if (!node)
+		new = malloc(sizeof(listint_t));
+		if (new == NULL)
 		{
-			*head = new;
-			new->next = NULL;
-			return (new);
+			return (NULL);
 		}
 		else
 		{
-			while (node)
+			for (j = 0; j < idx - 1; j++)
 			{
-				if (i == idx)
-				{
-					new->n = n;
-					new->next = node;
-					for (j = 0; j < idx - 1; j++)
-					{
-						p_node = p_node->next;
-					}
-					p_node->next = new;
-					return (new);
-				}
-				else
-				{
-					node = node->next;
-					i++;
-				}
+				p_node = p_node->next;
 			}
-			return (NULL);
+			new->n = n;
+			new->next = (p_node->next)->next;
+			p_node->next = new;
+			return (new);
 		}
 	}
+}
+/**
+ *add_nodeint - prints number of elements in a linked
+ *@head: input of type listint_t
+ *@n: input of type str
+ *Return: a new node of type listint_t
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		new_node->n = n;
+		new_node->next = *head;
+		*head = new_node;
+	}
+
+	return (new_node);
 }
