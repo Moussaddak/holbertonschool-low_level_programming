c5c765751bb08239dd6cc219f02c7d72ba794c25
moussaddak <meddeb.moussaddak@gmail.com>
2020-03-31 21:58:39 +0000

    header file

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HF_HOLBERTON
+#define HF_HOLBERTON
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+#endif
