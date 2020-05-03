f65e19cc4f351f90c3828304147fc68f96ede11d
Moussaddak <52888741+Moussaddak@users.noreply.github.com>
2020-05-03 23:45:15 +0200

    tasks

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -19,7 +19,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		new_node->n = n;
 		new_node->next = *head;
 		new_node->prev = NULL;
-		(*head)->prev = new_node;
+		if (*head)
+		{
+			(*head)->prev = new_node;
+		}
 		*head = new_node;
 	}
 
