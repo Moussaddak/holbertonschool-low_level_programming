6638aa3484c6d22da689472fe223974db825092a
moussaddak <meddeb.moussaddak@gmail.com>
2020-05-03 17:48:40 +0000

    tasks

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ *add_dnodeint - prints number of elements in a dlinked list
+ *@head: input of type dlistint_t
+ *@n: input of type integer
+ *Return: a new node of type dlistint_t
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+	{
+		return (NULL);
+	}
+	else
+	{
+		new_node->n = n;
+		new_node->next = *head;
+		new_node->prev = NULL;
+		*head = new_node;
+	}
+
+	return (new_node);
+}
