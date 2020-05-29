89f28faa4d4e4f07efb89a84d4c2d21c5bde1774
moussaddak <meddeb.moussaddak@gmail.com>
2020-05-29 23:32:24 +0000

    tasks

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,25 +11,28 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int hash_key;
 	hash_node_t *node;
 
-	if (!(key && value))
+	if (!(key && value && ht))
 	{
 		return (0);
 	}
-	hash_key = key_index(key, ht->size);
-	if (!(ht->array[hash_key])->key)
+	hash_key = key_index((const unsigned char *)key, ht->size);
+
+	if (!ht->array[hash_key])
 	{
-		(ht->array[hash_key])->key = strdup(key);
-		(ht->array[hash_key])->value = strdup(value);
-		(ht->array[hash_key])->next = NULL;
+		node = malloc(sizeof(hash_node_t));
+		if (!node)
+			return (0);
+		node->key = strdup(key);
+		node->value = strdup(value);
+		node->next = NULL;
+		ht->array[hash_key] = node;
 		return (1);
 	}
 	else
 	{
 		node = malloc(sizeof(hash_node_t));
 		if (!node)
-		{
 			return (0);
-		}
 		else
 		{
 			node->key = strdup(key);
