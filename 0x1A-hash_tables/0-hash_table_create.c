2d3fca457e1dd2405214c38e64a4eda99bb10b2f
moussaddak <meddeb.moussaddak@gmail.com>
2020-05-29 21:27:52 +0000

    tasks

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,36 +10,29 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_node_t **h_array = NULL;
 	unsigned long int i;
 
-	if (size > 0)
-	{
-		table = malloc(sizeof(hash_table_t));
+	table = malloc(sizeof(hash_table_t));
 
-		if (!table)
+	if (!table)
+	{
+		return (NULL);
+	}
+	else
+	{
+		h_array = malloc(sizeof(hash_node_t) * size);
+		if (!h_array)
 		{
+			free(table);
 			return (NULL);
 		}
 		else
 		{
-			h_array = malloc(sizeof(hash_node_t) * size);
-			if (!h_array)
+			for (i = 0; i < size; i++)
 			{
-				free(table);
-				return (NULL);
+				h_array[i] = NULL;
 			}
-			else
-			{
-				for (i = 0; i < size; i++)
-				{
-					h_array[i] = NULL;
-				}
-				table->size = size;
-				table->array = h_array;
-				return (table);
-			}
+			table->size = size;
+			table->array = h_array;
+			return (table);
 		}
-	}
-	else
-	{
-		return (NULL);
 	}
 }
