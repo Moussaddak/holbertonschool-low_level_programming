ef4f54ca736f453261eb5e9248dce6486347574f
Moussaddak <52888741+Moussaddak@users.noreply.github.com>
2020-05-30 23:52:32 +0200

    tasks

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,6 +6,7 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i, flag = 1;
+	hash_node_t *tmp = NULL;
 
 	if (!ht)
 		return;
@@ -14,14 +15,21 @@ void hash_table_print(const hash_table_t *ht)
 	{
 		if (ht->array[i] && flag)
 		{
-			flag = 0;
+			tmp = ht->array[i]->next, flag = 0;
 			printf("'%s': '%s'", ht->array[i]->key,
 			       ht->array[i]->value);
 		}
 		else if (ht->array[i] && !flag)
 		{
+			tmp = ht->array[i]->next;
 			printf(", '%s': '%s'", ht->array[i]->key,
 			       ht->array[i]->value);
+		}
+
+		while (tmp)
+		{
+			printf(", '%s': '%s'", tmp->key, tmp->value);
+			tmp = tmp->next;
 		}
 	}
 	printf("}\n");
