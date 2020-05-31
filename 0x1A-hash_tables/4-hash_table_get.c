6f15e99333124af07eadf6c1684e830f23b3c5c2
moussaddak <meddeb.moussaddak@gmail.com>
2020-05-31 22:33:16 +0000

    tasks

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,1 +1,28 @@
 #include "hash_tables.h"
+/**
+ *hash_table_get - retrieves a value associated with a key
+ *@ht: hash table
+ *@key: the key
+ *Return: Returns the value of key or NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int h_index;
+	hash_node_t *node;
+
+	h_index = key_index((const unsigned char *)key, ht->size);
+
+	node = ht->array[h_index];
+	/* search for the key */
+	while (node)
+	{
+		if (!strcmp(node->key, key))
+		{
+			return (node->value);
+		}
+		node = node->next;
+	}
+	/* if key not found or slot is null */
+
+	return (NULL);
+}
