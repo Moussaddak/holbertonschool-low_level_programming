0c09365a546b035eb698fe2c59e9e691ba3eaf1c
moussaddak <meddeb.moussaddak@gmail.com>
2020-05-31 16:18:44 +0000

    tasks

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,10 +20,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		node = malloc(sizeof(hash_node_t));
 		if (!node)
 			return (0);
-		node->key = strdup(key);
-		node->value = strdup(value);
+		node->key = strdup(key), node->value = strdup(value);
 		node->next = NULL;
 		tmp = node;
+
 	}
 	else
 	{
@@ -31,8 +31,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			if (!strcmp(tmp->key, key))
 			{
-				free(tmp->value);
-				tmp->value = strdup(value), break;
+				free(tmp->value), tmp->value = strdup(value);
+				break;
 			}
 			prev = tmp, tmp = tmp->next;
 			if (!tmp)
