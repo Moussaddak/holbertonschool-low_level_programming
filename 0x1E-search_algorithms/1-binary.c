96d04d36676d24768f84329155714c56e7beb73a
Moussaddak <52888741+Moussaddak@users.noreply.github.com>
2020-08-27 23:31:31 +0200

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -40,12 +40,12 @@ void print(int *array, size_t size)
 {
 	int i;
 
-	printf("Searching in array:");
+	printf("Searching in array: ");
 	for (i = 0; i < (int)size; i++)
 	{
-		printf(" %d", array[i]);
+		printf("%d", array[i]);
 		if (i != (int)size - 1)
-			printf(",");
+			printf(", ");
 	}
 	printf("\n");
 }
